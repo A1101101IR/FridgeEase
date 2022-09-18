e0620e278caa35f5bf81d3bc504f002140b41004
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-09-18 15:50:06 +0200

    working with fridge

--- src/components/fridge.js
+++ src/components/fridge.js
@@ -1,5 +1,16 @@
+import Plus from "../img/plus.png";
 const Fridge = () => {
-  return <h1>Fridge</h1>;
+  return (
+    <>
+      <section className="fridge">
+        <div className="fakeBTN">
+          <h4>Add Products</h4>
+          <img src={Plus} alt="" />
+        </div>
+        <p>Your fridge is empty! add products!</p>
+      </section>
+    </>
+  );
 };
 
 export default Fridge;
