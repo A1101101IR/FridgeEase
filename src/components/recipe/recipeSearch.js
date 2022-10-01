89bec84a94e4957d544c0c51b3bf45f8034e03cd
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-10-01 22:10:52 +0200

    working with recipe api

--- src/components/recipe/recipeSearch.js
+++ src/components/recipe/recipeSearch.js
@@ -11,24 +11,22 @@ const RecipeSearch = () => {
   const { data: productData } = useFatch(
     `http://localhost:8000/products/${id}`
   );
-  console.log(productData);
 
+  const searchWord = "bacon";
   useEffect(() => {
-    /* if (productData.name) {
-      fetch(
-        `https://edamam-recipe-search.p.rapidapi.com/search?q=${productData.name}`,
-        {
-          method: "GET",
-          headers: {
-            "X-RapidAPI-Key":
-              "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901",
-            "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
-          },
-        }
-      )
-        .then((res) => res.json())
-        .then((results) => setSearchResult(results.hits));
-    } */
+    fetch(
+      `https://edamam-recipe-search.p.rapidapi.com/search?q=${searchWord}`,
+      {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key":
+            "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901",
+          "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((results) => setSearchResult(results.hits));
     /* const searchWord = "bacon";
     fetch(
       `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=${searchWord}`,
@@ -46,27 +44,28 @@ const RecipeSearch = () => {
   }, []);
   return (
     <div className="recipe-container">
-      {productData && (
-        <article className="recipe-cart-small">
-          <header>
-            <div className="flex-center">
-              <img
-                src={require(`../../img/icons8/${productData.icon}`)}
-                className="product-icon"
-              />
-              <h4 className="cart-headline-bold">BACON BLABLA</h4>
+      {searchResult &&
+        searchResult.map((item) => (
+          <article className="recipe-cart-small">
+            <header>
+              <div className="flex-center">
+                <img
+                  src={require(`../../img/icons8/${productData.icon}`)}
+                  className="product-icon"
+                />
+                <h4 className="cart-headline-bold">{item.recipe.label}</h4>
+              </div>
+              <div className="flex-center">
+                <img className="favoriteBTN" src={Favorite} />
+              </div>
+            </header>
+            <div className="recipe-cart-body">
+              <div className="fakeIMG"></div>
+              <div className="info1"></div>
+              <div className="info2"></div>
             </div>
-            <div className="flex-center">
-              <img className="favoriteBTN" src={Favorite} />
-            </div>
-          </header>
-          <div className="recipe-cart-body">
-            <div className="fakeIMG"></div>
-            <div className="info1"></div>
-            <div className="info2"></div>
-          </div>
-        </article>
-      )}
+          </article>
+        ))}
     </div>
   );
 };
