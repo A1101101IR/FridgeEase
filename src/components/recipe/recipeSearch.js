e49313d21e3194c3ca82b317cd9eeb4ec1769ab5
Amir Nabizadeh <nabizadeh.se@gmail.com>
2022-10-02 16:40:19 +0200

    working with recipe and loading, error

--- src/components/recipe/recipeSearch.js
+++ src/components/recipe/recipeSearch.js
@@ -1,71 +1,87 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import useFatch from "../customHooks/useFetch";
 import Recipe from "../recipe";
 import Favorite from "../../img/favorite.png";
-
+import loadingImg from "../../img/loading.png";
 const RecipeSearch = () => {
   const { id } = useParams();
-  const [searchResult, setSearchResult] = useState();
-  const { data: productData } = useFatch(
-    `http://localhost:8000/products/${id}`
-  );
-
   const searchWord = "bacon";
+  const [searchResult, setSearchResult] = useState();
+  const {
+    data: productData,
+    isLoading,
+    error,
+  } = useFatch(`http://localhost:8000/products/${id}`);
+  console.log(productData);
+
   useEffect(() => {
-    fetch(
-      `https://edamam-recipe-search.p.rapidapi.com/search?q=${searchWord}`,
-      {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901",
-          "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((results) => setSearchResult(results.hits));
-    /* const searchWord = "bacon";
-    fetch(
-      `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=${searchWord}`,
-      {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901",
-          "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((results) => console.log(results)); */
-  }, []);
+    if (!isLoading) {
+      fetch(
+        `https://edamam-recipe-search.p.rapidapi.com/search?q=${productData.name}`,
+        {
+          method: "GET",
+          headers: {
+            "X-RapidAPI-Key":
+              "fc6f5f41bfmshb50a75999c65417p13ac81jsn45483f53f901",
+            "X-RapidAPI-Host": "edamam-recipe-search.p.rapidapi.com",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((results) => setSearchResult(results.hits));
+    }
+  }, [isLoading]);
   return (
     <div className="recipe-container">
-      {searchResult &&
-        searchResult.map((item) => (
-          <article className="recipe-cart-small">
-            <header>
-              <div className="flex-center">
-                <img
-                  src={require(`../../img/icons8/${productData.icon}`)}
-                  className="product-icon"
-                />
-                <h4 className="cart-headline-bold">{item.recipe.label}</h4>
-              </div>
-              <div className="flex-center">
-                <img className="favoriteBTN" src={Favorite} />
-              </div>
-            </header>
-            <div className="recipe-cart-body">
-              <div className="fakeIMG"></div>
-              <div className="info1"></div>
-              <div className="info2"></div>
-            </div>
-          </article>
-        ))}
+      {isLoading && (
+        <div className="loading-box">
+          <img src={loadingImg} className="loading" />
+          <h4>Loading...</h4>
+        </div>
+      )}
+      {error && <h4>obs...</h4>}
+      {!isLoading && (
+        <>
+          {!error && (
+            <>
+              {searchResult &&
+                searchResult.map((item) => (
+                  <article className="recipe-cart-small">
+                    <header>
+                      <div className="flex-center">
+                        <img
+                          src={require(`../../img/icons8/${productData.icon}`)}
+                          className="product-icon"
+                        />
+                        <h4 className="cart-headline-bold">
+                          {item.recipe.label}
+                        </h4>
+                      </div>
+                      <div className="flex-center">
+                        <img className="favoriteBTN" src={Favorite} />
+                      </div>
+                    </header>
+                    <div className="recipe-cart-body">
+                      <div className="meal-dish-type">
+                        <span>{item.recipe.mealType}</span>
+                        <span>{item.recipe.dishType}</span>
+                      </div>
+                      <div className="recipe-detail">
+                        <ul>
+                          {item.recipe.ingredients.map((ingredient) => (
+                            <li>{ingredient.text}</li>
+                          ))}
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                ))}
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };
